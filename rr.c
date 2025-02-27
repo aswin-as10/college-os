c710ebfc7b61107cdd25ea51002d63b368df49ce
aswin-as10 <aswinas047@gmail.com>
2025-02-27 21:58:51 +0530

    rr.c

---
+++ rr.c
@@ -0,0 +1,65 @@
+
+#include <stdio.h>
+
+int main() {
+    int n, i, tq, time = 0, done;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    
+    int bt[n], wt[n], tat[n], remaining_bt[n];
+    
+    
+    printf("Enter burst time for each process:\n");
+    for (i = 0; i < n; i++) {
+        printf("Process %d: ", i + 1);
+        scanf("%d", &bt[i]);
+        remaining_bt[i] = bt[i]; 
+        wt[i] = 0;  
+        tat[i] = 0;  
+    }
+    
+   
+    printf("Enter time quantum: ");
+    scanf("%d", &tq);
+
+    
+    while (1) {
+        done = 1;
+        for (i = 0; i < n; i++) {
+            if (remaining_bt[i] > 0) {
+                done = 0;
+                if (remaining_bt[i] > tq) {
+                    time += tq;
+                    remaining_bt[i] -= tq;
+                } else {
+                    time += remaining_bt[i];
+                    wt[i] = time - bt[i];
+                    remaining_bt[i] = 0;
+                }
+            }
+        }
+        if (done) break;
+    }
+
+   
+    for (i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i]; 
+    }
+
+    
+    float total_wt = 0, total_tat = 0;
+    
+ 
+    printf("\nProcess\tBT\tWT\tTAT\n");
+    for (i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\t%d\n", i + 1, bt[i], wt[i], tat[i]);
+        total_wt += wt[i];
+        total_tat += tat[i];
+    }
+
+   
+    printf("\nAverage Waiting Time: %.2f\n", total_wt / n);
+    printf("Average Turnaround Time: %.2f\n", total_tat / n);
+
+    return 0;
+}
