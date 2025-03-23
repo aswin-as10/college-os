4e39988abb9b292951c4b9d17777b2d96d57e2c9
aswin-as10 <aswinas047@gmail.com>
2025-03-23 22:56:11 +0530

    fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -1,46 +1,45 @@
 #include<stdio.h>
-int p[30],bt[30],tot_tat=0,wt[30],n,tot_wt=0,tat[30],FCFS_wt=0,FCFS_tat=0;
-float awt,avg_tat,avg_wt;
-int WT_TAT(int*a,int*b);
-void main()
-{
-  int i;
-  printf("\nenter the no of processes\n");
-  scanf("%d",&n);
-  printf("enter burst time for each process\n");
-  for(i=0;i<n;i++)
-     {
-    scanf("%d",&bt[i]);
-    p[i]=i;
-   }
-    printf("\n FCfS algorithm \n");
-    WT_TAT(&FCFS_tat,&FCFS_wt);
-    printf("\n\nToatal turn around time :%d",FCFS_tat);
-    printf("\n average turn around time :%d",FCFS_tat);
-    printf("\n total waitng time :%d",FCFS_wt);
-    printf("\naverge waiting time :%d",FCFS_wt);
+int n,p[30],bt[30],tot_tat=0,wt[30],tot_wt=0,tat[30],fcfs_tat=0,fcfs_wt=0;
+float avg_wt=0,avg_tat=0;
+int wt_tat(int *a,int *b);
+int main(){
+    printf("enter the no of processes\n");
+    scanf("%d",&n);
+    printf("enter the burst time of each processes\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&bt[i]);
+        p[i]=i;
+    }
+    printf("\nFCFS ALGORITHM\n");
+    wt_tat(&fcfs_tat,&fcfs_wt);
+    avg_tat=(float)fcfs_tat/n;
+    avg_wt=(float)fcfs_wt/n;
+    printf("\naverage turn around time:%.2f",avg_tat);
+    printf("\ntotal turn around time:%d",fcfs_tat);
+    printf("\naverage waiting time:%.2f",avg_wt);
+    printf("\ntotal waiting time:%d",fcfs_wt);
+
 }
-int WT_TAT(int*a,int*b)
- {
-  int i;
-    for(i=0;i<n;i++){
-       if(i==0)
-         tat[i]=bt[i];
-       else
-         tat[i]=tat[i-1]+bt[i];
-         tot_tat=tot_tat+tat[i];
+int wt_tat(int *a,int *b){
+     int i;
+     for(i=0;i<n;i++){
+        if(i==0)
+            tat[i]=bt[i]; 
+        else
+          tat[i]=tat[i-1]+bt[i];
+        tot_tat=tot_tat+tat[i];
      }
      *a=tot_tat;
      wt[0]=0;
-  for(i=0;i<n;i++)
-   {
+     for(i=0;i<n;i++){
         wt[i]=wt[i-1]+bt[i-1];
         tot_wt=tot_wt+wt[i];
-  }
-  *b=tot_wt;
-  printf("\n PROCESS\t\tBURST TIME\tTURN AROUND TIME \tWAITING TIME");
-  for(i=0;i<n;i++)
-    
-       printf("\nprocess[%d]\t\t%d\t\t%d\t\t%d",p[i],bt[i],tat[i],wt[i]);
-        return 0;
+     }
+     *b=tot_wt;
+     printf("\nPROCESS\t\tBURST TIME\t\tTURNAROUND TIME\t\tWAITING TIME");
+     for(i=0;i<n;i++){
+        printf("\np[%d]\t\t%d\t\t\t\t%d\t\t%d",p[i],bt[i],tat[i],wt[i]);
+        
+     }
+     return 0;
 }
