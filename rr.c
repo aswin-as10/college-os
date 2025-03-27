21e5184b97660b34bc945024aa67a25a9c051932
aswin-as10 <aswinas047@gmail.com>
2025-03-27 20:53:16 +0530

    rr.c

--- rr.c
+++ rr.c
@@ -1,58 +1,59 @@
-#include <stdio.h>
-
-int main() {
-    int n, i, tq, time = 0, done;
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-    
-    int bt[n], wt[n], tat[n], remaining_bt[n], original_bt[n];
-    
-    printf("Enter burst time for each process:\n");
-    for (i = 0; i < n; i++) {
-        printf("Process %d: ", i + 1);
-        scanf("%d", &bt[i]);
-        remaining_bt[i] = bt[i];  
-        original_bt[i] = bt[i];   
-        wt[i] = 0;  
-        tat[i] = 0;  
+#include<stdio.h>
+int tq,n,tat[30],wt[30],tot_tat=0,tot_wt=0,rr_tat=0,rr_wt=0,p[30],bt[30],rem_bt[30],quantum;
+float avg_wt,avg_tat;
+int wt_tat(int*a,int*b);
+int main(){
+    printf("\nenter the no of process");
+    scanf("%d",&n);
+    printf("\nenter the burst time of procesess");
+    for(int i=0;i<n;i++){
+        scanf("%d",&bt[i]);
+        rem_bt[i]=bt[i];
+        p[i]=i;
     }
-    
-    printf("Enter time quantum: ");
-    scanf("%d", &tq);
-
-    while (1) {
-        done = 1;
-        for (i = 0; i < n; i++) {
-            if (remaining_bt[i] > 0) {
-                done = 0;
-                if (remaining_bt[i] > tq) {
-                    time += tq;
-                    remaining_bt[i] -= tq;
-                } else {
-                    time += remaining_bt[i];
-                    wt[i] = time - original_bt[i]; 
-                    remaining_bt[i] = 0;
+    printf("\nenter the time slice");
+    scanf("%d",&tq);
+    printf("\nROUND ROBIN");
+    wt_tat(&rr_tat,&rr_wt);
+    avg_tat=(float)rr_tat/n;
+    avg_wt=(float)rr_wt/n;
+    printf("\naverage turn around time:%.2f",avg_tat);
+    printf("\ntotal turn around time:%d",rr_tat);
+    printf("\naverage waiting time:%.2f",avg_wt);
+    printf("\ntotal waiting time:%d",rr_wt);
+}
+int wt_tat(int *a,int *b){
+    int t=0,remaining_pr=n;
+    while(remaining_pr>0)
+    {
+        for(int i=0;i<n;i++)
+        {
+              if(rem_bt[i]>0)
+              {
+                if(rem_bt[i]>tq){
+                    t+=tq;
+                    rem_bt[i]-=tq;
                 }
-            }
+                else{
+                    t+=rem_bt[i];
+                    tat[i]=t;
+                    rem_bt[i]=0;
+                    remaining_pr--;
+                   }
+              }
         }
-        if (done) break; 
     }
-
-    for (i = 0; i < n; i++) {
-        tat[i] = original_bt[i] + wt[i]; 
+    for(int i=0;i<n;i++){
+        wt[i]=tat[i]-bt[i];
+        tot_tat+=tat[i];
+        tot_wt+=wt[i];
     }
-
-    float total_wt = 0, total_tat = 0;
-    
-    printf("\nProcess\tBT\tWT\tTAT\n");
-    for (i = 0; i < n; i++) {
-        printf("%d\t%d\t%d\t%d\n", i + 1, original_bt[i], wt[i], tat[i]);
-        total_wt += wt[i];
-        total_tat += tat[i];
+    *a=tot_tat;
+    *b=tot_wt;
+    printf("\nPROCESS\tBURST TIME\tTURNAROUND TIME\tWAITING TIME");
+    for (int i = 0; i < n; i++) {
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d", p[i], bt[i], tat[i], wt[i]);
     }
-
-    printf("\nAverage Waiting Time: %.2f\n", total_wt / n);
-    printf("Average Turnaround Time: %.2f\n", total_tat / n);
 
     return 0;
 }
